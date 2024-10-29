d2e8c480478ce3fed3a95b5f96c773f129866f73
jaydeepsenma <rawatjay532@gmail.com>
2024-10-29 12:00:35 +0530

    shift mongodb to online server

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app     = express();
 const db      = require('./db');
-
+require('dotenv').config();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // req.body
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 });
+
+const PORT = process.env.PORT || 3000;
 
 // Import the router files
 const personRoutes = require('./routes/personRoutes');
@@ -17,4 +19,5 @@ const menuItemRoutes = require('./routes/menuItemRoutes');
 app.use('/person',personRoutes);
 app.use('/menu',menuItemRoutes);
 
-app.listen(3000);
+
+app.listen(PORT);
